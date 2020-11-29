8b6f262be5f7b0229adfb33a913fc5590fe099ef
Christoph Stade <christoph-stade@outlook.de>
2020-11-29 19:59:46 +0100

    added all the file folders

--- Praktikum5/Program.cs
+++ Praktikum5/Program.cs
@@ -7,7 +7,7 @@ namespace Praktikum5
         static void Main(string[] args)
         {
             Console.WriteLine("Pascalsches Dreieck:");
-            int[] f = { 1 };
+            int[] f = { 1, 2, 2, 1 };
 
             for (int i = 0; i < 8; i++)
             {
@@ -18,79 +18,74 @@ namespace Praktikum5
             Console.WriteLine();
             Console.WriteLine("Primzahlen bis 20");
 
-            Primzahlen(20, true);
+            Primzahlen(120, true);
             const int Primzahlgrenze = 750000;
 
             Console.WriteLine($"Anzahl der Primzahlen bis {Primzahlgrenze}: {Primzahlen(Primzahlgrenze)}");
         }
 
-        static int[] PascalDreieck(int[] f)
+        static int[] PascalDreieck(int[] f = null)
         {
-            int[] ergebnis = new int[f.Length + 1];
-
-            if (f.Length == 1)
-                Console.WriteLine(f[0]);
-
-            for (int i = 0; i < f.Length; i++)
+            if (f == null) // wenn keine Eingabe aka null uebergeben wurde, wird f mit 1 initialisiert
             {
-                if (i == 0)
-                {
-                    ergebnis[i] = f[i];
-                }
-                else
-                {
-                    ergebnis[i] = f[i - 1] + f[i];
-                }
+                f = new int[]{ 1 };
+                return f;
             }
-            ergebnis[^1] = f[^1];
+
+            int[] ergebnis = new int[f.Length + 1]; // Array mit einem zusaetzlichen Element
+
+            ergebnis[0] = 1; // linker Rand ist immer 1
+
+            for (int i = 1; i < f.Length; i++) // geht alle Elemente des Input Array durch
+            {
+                ergebnis[i] = f[i - 1] + f[i]; 
+            }
+            ergebnis[^1] = 1; // rechter Rand ist immer 1
 
             return ergebnis;
         }
 
         static void Ausgabe(int[] f)
         {
-            foreach (int element in f)
+            foreach (int element in f) // gibt alle Element aus f in einer Zeile aus
             {
-                Console.Write(element);
+                Console.Write($"{element} ");
             }
             Console.WriteLine();
         }
 
         static int Primzahlen(int n, bool ausgabe = false)
         {
-            bool[] primeBool = new bool[n];
+            bool[] primeBool = new bool[n + 1]; // Array mit einem Element mehr da 0 bei den Zahlen dabei ist
 
-            for (int i = 0; i < primeBool.Length; i += 2)
+            for (int i = 3; i < primeBool.Length; i += 2) // alle ungeraden Zahlen auf true | alle geraden auf false
             {
-                primeBool[i] = false;
-
-                if (i > 2)
-                    primeBool[i - 1] = true;
+                primeBool[i] = true;
             }
 
-            primeBool[1] = false;
-            primeBool[2] = true;
+            primeBool[2] = true; // 2 wird auf true gesetzt da die Schleife oben nur die ungeraden auf true gesetzt hat
 
-            for (int j = 3; j < primeBool.Length; j += 2)
+            int primzahlCounter = 1; // wegen der 2 schon auf 1
+
+            if (ausgabe)
+                Console.Write("2");
+
+            for (long j = 3; j < primeBool.Length; j += 2) // alle ungeraden Zahlen werden geprueft
             {
-                for (int k = 5; k < primeBool.Length; k += 2)
+                if (primeBool[j]) // prueft ob Primzahl
                 {
-                    if (primeBool[j] != false)
-                    {
-                        if (k % j == 0)
+                    primzahlCounter += 1;
+
+                    if (ausgabe)
+                        Console.Write($"   {j}");
+                    for (long k = (long)Math.Pow(j, 2); k < primeBool.Length; k += 2) // streicht alle vielfachen von der Primzahl j
+                        if (primeBool[j] && (k % j == 0)) // Bedingung keine Primzahl zu sein
                             primeBool[k] = false;
-                    }
                 }
             }
 
-            int counter = 1; // wegen der 2 schon auf 1
-
-            for ( int l = 3; l < primeBool.Length; l += 2) // alle geraden Zahlen werden uebersprungen
-            {
-                counter += 1;
-            }
-            Console.WriteLine(counter);
-            return counter;
+            Console.Write("\n");
+            return primzahlCounter;
         }
     }
 }
---
+++ Praktikum6/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+
+namespace Praktikum6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int maxWrongGuesses = 5;
+            var wrongGuesses = 0;
+            var word = GetWord();
+            var wordGuessed = false;
+            var secretWord = "";
+            var guessedChars = "";
+
+            Console.WriteLine(" Hangman\n =======");
+            Console.WriteLine($" Gesucht ist ein Wort mit {word.Length} Buchstaben.");
+
+            for (int i = 0; i < word.Length; i++)
+                secretWord += "-";
+            var secretWordCharArray = secretWord.ToCharArray();
+
+            while (wrongGuesses < maxWrongGuesses && !wordGuessed)
+                Guess(maxWrongGuesses, ref wrongGuesses, word, ref secretWordCharArray, ref wordGuessed, ref guessedChars);
+
+            if (wordGuessed)
+                Console.WriteLine(" Du hast gewonnen!");
+            else
+                Console.WriteLine(" Du hast verloren!");
+        }
+
+        /// <summary>
+        /// Returns a random word from aan array.
+        /// </summary>
+        /// <returns></returns>
+        static string GetWord()
+        {
+            string[] words = {"Programmieren",
+                              "Praktikum",
+                              "Nuernberg",
+                              "Informatik",
+                              "Professor",
+                              "Hangman",
+                              "Visualstudio",
+                              "Csharp",
+                              "Student",
+                              "Mensateria"};
+
+            Random random = new Random();
+            string rndWord = words[random.Next(0, words.Length)];
+            return rndWord;
+        }
+        /// <summary>
+        /// writes a given word on the console.
+        /// </summary>
+        /// <param name="word"></param>
+        static void PrintWord(string word)
+        {
+            Console.WriteLine("\n Das geheime Wort ist: " + word);
+        }
+        /// <summary>
+        /// prints every letter in a word.
+        /// </summary>
+        /// <param name="word"></param>
+        static void PrintChars(string word)
+        {
+            Console.Write(" Falsche Buchstaben: ");
+            foreach (var element in word)
+            {
+                Console.Write(element + ", ");
+            }
+        }
+        /// <summary>
+        /// plays one round (1 letter) of hangman.
+        /// </summary>
+        /// <param name="maxGuesses"></param>
+        /// <param name="wrongGuesses"></param>
+        /// <param name="word"></param>
+        /// <param name="secretWordCharArray"></param>
+        /// <param name="wordGuessed"></param>
+        /// <param name="guessedChars"></param>
+        static void Guess(int maxGuesses, ref int wrongGuesses, string word, ref char[] secretWordCharArray, ref bool wordGuessed, ref string guessedChars)
+        {
+            PrintWord(new String(secretWordCharArray));
+            PrintChars(guessedChars);
+            Console.Write("\n Buchstaben eingeben: ");
+
+            var charGuessed = false;
+            var guess = Console.ReadKey().KeyChar;
+            var wordCharArray = word.ToCharArray();
+            var charWasAlreadyGuessed = false;
+
+            for (int i = 0; i < wordCharArray.Length; i++) // checks every letter in the word
+            {
+                if (Char.ToLower(guess) == Char.ToLower(wordCharArray[i])) // Hit!
+                {
+                    if (!charGuessed) // pro Buchstabe nur einmal "ist richtig" ausgeben
+                        Console.Write($"--> {guess} ist richtig!");
+
+                    secretWordCharArray[i] = wordCharArray[i];
+                    charGuessed = true;
+                }
+
+                foreach (var element in guessedChars) // checks if character was already falsely guessed
+                    if (element == guess)
+                        charWasAlreadyGuessed = true;
+            }
+            if (!charGuessed && !charWasAlreadyGuessed) // Wrong Guess.
+            {
+                wrongGuesses++;
+
+                guessedChars += guess;
+                Console.Write($"--> {guess} ist falsch!");
+                Console.WriteLine($" {wrongGuesses} von {maxGuesses} Fehlern.");
+            }
+            else if (secretWordCharArray.ToString() == word) // Game won.
+            {
+                wordGuessed = true;
+                PrintWord(word);
+            }
+        }
+    }
+}

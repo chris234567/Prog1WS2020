a6c6bb0ed70999e02f89fd67bb421724363d5831
Christoph Stade <christoph-stade@outlook.de>
2020-12-01 14:48:02 +0100

    added Pr1 and Pr2

---
+++ Praktikum1/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Globalization;
+using System.Runtime.Intrinsics;
+
+namespace _1PraktikumsaufgabeOnlineShop
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            double pfefferMenge;
+            double paprikaMenge;
+            double curryMenge;
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;  // enable unicode output
+
+            Console.WriteLine("Herzlich Wilkommmen bei Scharfe Küche!");
+            Console.Write("Pfeffer   (à 5,10€):   ");
+            pfefferMenge = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Paprika   (à 3,90€):   ");
+            paprikaMenge = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Curry     (à 4,50€):   ");
+            curryMenge = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("");
+
+            double preis = pfefferMenge * 5.10 + paprikaMenge * 3.90 + curryMenge * 4.50;
+
+            if (preis <= 10)    // Mindestbestellwertabfrage
+            {
+                Console.WriteLine("Ihre Bestellung erreicht nicht den Mindestbestellwert von 10,00 Euro");
+            }
+            else if (25 < preis && preis < 30)    // Abfrage ob Preis im kritischen Bereich
+            {
+
+                double dif = (30 - preis);
+
+                Console.WriteLine("{0,1} {1,1:N2} {2,10}", "Ihnen fehlen nur noch", dif, "Euro bis zum kostenfreien Versand!");
+                Console.WriteLine("Wollen Sie noch eine Dose pfefferMenge mehr bestellen und den Versand");
+                Console.Write("sparen(ja | nein) ?");
+
+                String answer = Console.ReadLine();
+
+                if (answer == "ja")    // Abrage ob Versand gespart werden soll
+                {
+                    pfefferMenge += 1;
+                }
+
+                // Bestellbestätigung
+
+                preis = pfefferMenge * 5.10 + paprikaMenge * 3.90 + curryMenge * 4.50;
+
+                Console.WriteLine(" ");
+                Console.WriteLine("Bestellbestätigung durch Scharfe Küche:");
+
+                double pfefferMengepreis = pfefferMenge * 5.10;
+                double paprikaMengepreis = paprikaMenge * 3.90;
+                double curryMengepreis = curryMenge * 4.50;
+
+                Console.WriteLine($"{"Pfeffer",1} {pfefferMenge,25} {"x 5,10",3} {pfefferMengepreis,17:N2} {"EUR"}");
+                Console.WriteLine($"{"Paprika",1} {paprikaMenge,25} {"x 5,10",3} {paprikaMengepreis,17:N2} {"EUR"}");
+                Console.WriteLine($"{"Curry",1} {curryMenge,27} {"x 5,10",3} {curryMengepreis,17:N2} {"EUR"}");
+                Console.WriteLine("‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐-----");
+                Console.WriteLine($"{"Zwischensumme",1} {preis,44:N2} {"EUR",-1}");
+
+                double mwst = (preis / 116 * 16);
+
+                Console.WriteLine($"{"Enthaltene MwSt (16%)",1} {mwst,36:N2} {"EUR",-1}");
+
+                if (preis <= 30)    // Versandberechnung
+                {
+                    Console.WriteLine("Versandpauschale                                      3,90 EUR");
+                    Console.WriteLine("‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐-----");
+
+                    double sum = (preis + 3.90);
+
+                    Console.WriteLine($"{"Summe",1} {sum,52:N2} {"EUR",-1}");
+                }
+                else
+                {
+                    Console.WriteLine("Versandpauschale                                      0,00 EUR");
+                    Console.WriteLine("‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐-----");
+                    Console.WriteLine($"{"Summe",1} {preis,52:N2} {"EUR",-1}");
+                }
+            }
+            else
+            {
+                // Bestellbestätigung
+
+                preis = pfefferMenge * 5.10 + paprikaMenge * 3.90 + curryMenge * 4.50;
+
+                Console.WriteLine(" ");
+                Console.WriteLine("Bestellbestätigung durch Scharfe Küche:");
+
+                double pfefferMengepreis = pfefferMenge * 5.10;
+                double paprikaMengepreis = paprikaMenge * 3.90;
+                double curryMengepreis = curryMenge * 4.50;
+
+                Console.WriteLine($"{"Pfeffer",1} {pfefferMenge,25} {"x 5,10",3} {pfefferMengepreis,17:N2} {"EUR"}");
+                Console.WriteLine($"{"Paprika",1} {paprikaMenge,25} {"x 5,10",3} {paprikaMengepreis,17:N2} {"EUR"}");
+                Console.WriteLine($"{"Curry",1} {curryMenge,27} {"x 5,10",3} {curryMengepreis,17:N2} {"EUR"}");
+                Console.WriteLine("‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐-----");
+                Console.WriteLine($"{"Zwischensumme",1} {preis,44:N2} {"EUR",-1}");
+
+                double mwst = (preis / 116 * 16);
+
+                Console.WriteLine($"{"Enthaltene MwSt (16%)",1} {mwst,36:N2} {"EUR",-1}");
+
+                if (preis <= 30)    // Versandberechnung
+                {
+                    Console.WriteLine("Versandpauschale                                      3,90 EUR");
+                    Console.WriteLine("‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐-----");
+
+                    double sum = (preis + 3.90);
+
+                    Console.WriteLine($"{"Summe",1} {sum,52:N2} {"EUR",-1}");
+                }
+                else
+                {
+                    Console.WriteLine("Versandpauschale                                      0,00 EUR");
+                    Console.WriteLine("‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐-----");
+                    Console.WriteLine($"{"Summe",1} {preis,52:N2} {"EUR",-1}");
+                }
+
+            }
+        }
+    }
+}
---
+++ Praktikum2/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+namespace Praktikum2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Erfragt eine Zahl größer gleich 10 und kleiner als 10000
+
+            Console.Write("Geben Sie eine Zahl größer gleich 10 oder kleiner als 10.000 ein: ");
+            int eingabe = int.Parse(Console.ReadLine());
+
+            while (eingabe < 10 || eingabe >= 10000)
+            {
+                Console.WriteLine("Ungültige Eingabe, bitte wiederholen.\n");
+                Console.Write("Geben Sie eine Zahl größer gleich 10 oder kleiner als 10.000 ein: ");
+                eingabe = int.Parse(Console.ReadLine());
+            }
+            
+            // Errechnet alle kleineren Zahlen als die Eingabe, die durch 50 oder 500 teilbar sind 
+
+            int teiler;
+
+            if (eingabe < 1000)
+            {
+                teiler = 50;
+            }
+            else
+            {
+                teiler = 500;
+            }
+
+            int num = eingabe;
+
+            Console.WriteLine($"{teiler}-er kleiner als " + num + ":");
+
+            // Berechnet alle echten Teiler der Eingabe -
+
+            while (num > 0)
+            {
+
+                if ((num / teiler) * teiler != 0)
+                {
+                    Console.WriteLine((num / teiler) * teiler);
+                }
+
+                num -= teiler;
+            }
+
+            Console.WriteLine("\nTeiler von " + eingabe + ":");
+
+            int sum = 0;
+
+            for (int i = eingabe - 1; i > 1; i--)
+            {
+                if ((eingabe % i) == 0)
+                {
+                    Console.WriteLine(i);
+                    sum = sum + i;
+                }
+            }
+
+            // - und gibt ihre Summe aus (oder gegebenenfalls, dass sie eine Primzahl ist)
+
+            if (sum != 0)
+            {
+                Console.WriteLine("\nDie Summe aller echten Zeiler von " + eingabe + " ist " + sum);
+            }
+            else
+            {
+                Console.WriteLine($"{eingabe} ist eine Primzahl.");
+            }
+
+            // Berechnet die Anzahl der ungeraden und geraden Stellen aus
+
+            int ungeradeStelle = 0;
+            int geradeStelle = 0;
+
+            int stellen = 0;
+
+            if (eingabe >= 10 && eingabe < 100)
+            {
+                stellen = 1;
+            }
+            else if (eingabe < 1000)
+            {
+                stellen = 2;
+            }
+            else if (eingabe >= 1000 && eingabe < 10000)
+            {
+                stellen = 3;
+            }
+
+            for (int i = 0; i <= stellen; i++)
+            {
+                int stelle = eingabe / (int)Math.Pow(10, i);
+
+                if ((stelle % 2) == 0)
+                {
+                    geradeStelle += 1;
+                }
+                else
+                {
+                    ungeradeStelle += 1;
+                }
+            }
+
+            Console.WriteLine($"\nDie Zahl {eingabe} hat {geradeStelle} gerade und {ungeradeStelle} ungerade Ziffern.");
+
+            // Berechnet das Produkt der ersten und letzten Ziffer der Eingabe
+
+            int potenz = 0;
+
+            if (eingabe < 100)
+            {
+                potenz = 1;
+            }
+            else if (eingabe >= 100 && eingabe < 1000)
+            {
+                potenz = 2;
+            }
+            else if (eingabe >= 1000 && eingabe < 10000)
+            {
+                potenz = 3;
+            }
+
+            int erstZiff = eingabe / (int)Math.Pow(10, potenz);
+            int letztZiff = eingabe % 10;
+            int sumZiff = erstZiff * letztZiff;
+
+            Console.WriteLine($"\nDas Produkt der ersten und letzten Ziffer von {eingabe} ist {erstZiff}*{letztZiff}={sumZiff}.");
+
+        }
+    }
+}

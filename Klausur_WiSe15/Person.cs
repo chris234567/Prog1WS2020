20cf6d872218790f3f2d965d84931a16f189af55
Christoph Stade <christoph-stade@outlook.de>
2021-01-17 18:18:58 +0100

    added some excersizes

---
+++ Klausur_WiSe15/Person.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Klausur_WiSe15
+{
+    public enum MTyp
+    {
+        Angestellter,
+        Gruppenleiter,
+        Abteilungsleiter,
+    }
+
+    class Person
+    {
+        string name;
+        double gehalt;
+        MTyp mtyp;
+        int personalnummer = 0;
+
+        public int NaechstPersNr
+        {
+            get
+            {
+                return personalnummer + 1;
+            }
+            set
+            {
+                personalnummer = value >= NaechstPersNr ? value : throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public Person(string name, double gehalt, MTyp mtyp, int personalnummer = -1)
+        {
+            this.name = name;
+            this.gehalt = gehalt;
+            this.mtyp = mtyp;
+            this.personalnummer = personalnummer != -1 ? personalnummer : NaechstPersNr;
+        }
+
+        public static double Gehaltvolumen(Person[] personen, MTyp mtyp)
+        {
+            double sum = 0;
+
+            for (int i = 0; i < personen.Length; i++)
+                if (personen[i].mtyp == mtyp)
+                    sum += personen[i].gehalt;
+
+            return sum;
+        }
+    }
+}
---
+++ Klausur_WiSe15/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Klausur_WiSe15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1
+
+            Console.WriteLine(RemoveBlanks("Hallo Welt          und hello      World"));
+
+            // 2
+
+            ZiffernZaehlen(1636);
+
+            // 3
+
+            var personen = new Person[2];
+            personen[0] = new Person("erster", 243.234, MTyp.Abteilungsleiter, 2131);
+            personen[1] = new Person("zweiter", 4564.1, MTyp.Angestellter);
+
+            Console.WriteLine(Person.Gehaltvolumen(personen, MTyp.Abteilungsleiter));
+        }
+
+        public static string RemoveBlanks(string s)
+        {
+            string tmp = "";
+
+            if (s != "" && s != null)
+                tmp += s[0];
+            else
+                return "Uebergebener String war Null bzw. leer!";
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                if (!(s[i] == ' ' && s[i - 1] == ' '))
+                    tmp += s[i];
+            }
+            return tmp;
+        }
+
+        public static void ZiffernZaehlen(int i)
+        {
+            var myArray = new int[10];
+
+            // max 4 stellen da der konzertsaal 3250 plaetze hat
+
+            myArray[i / 1000]++; // tausender
+            myArray[(i % 1000) / 100]++; // hunderter
+            myArray[(i % 100) / 10]++; // zehner
+            myArray[i % 10]++; // einer - stelle
+
+            for (int r = 0; r < myArray.Length; r++)
+            {
+                Console.WriteLine($"fuer die {r}er werden {myArray[r]} Schilder benoetigt");
+            }
+        }
+
+
+    }
+}
